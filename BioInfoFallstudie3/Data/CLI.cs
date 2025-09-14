f4e80cddc86cf1794bca3284e1a173b80fbe85e4
Tobias Kronser <skysworddev@gmail.com>
2025-09-14 22:41:10 +0200

    message

--- BioInfoFallstudie3/Data/CLI.cs
+++ BioInfoFallstudie3/Data/CLI.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BioInfoFallstudie3.Data;
+//namespace BioInfoFallstudie3.Data;
 
-public static class CLI
-{
+//public static class CLI
+//{
 
-    public static void Run()
-    {
-        Console.WriteLine("Bitte eine Datei zu einem Hidden Markov Model eingeben:");
-    }
+//    public static void Run()
+//    {
+//        Console.WriteLine("Pfad zur Datei mit hidden states (1 je Zeile):");
+//        string hiddenStatePath = Console.ReadLine()!;
+//        string[] hiddenStates = File.ReadAllLines(hiddenStatePath).Select(;
 
-}
+//        Console.WriteLine("Found");
+//        foreach(string hiddenState in hiddenStates)
+//        {
+//            Console.Write(hiddenState);
+//            Console.Write(" ");
+//        }
+//        Console.WriteLine();
+//    }
+
+//}
